5c0e2d9b5da1da2ec3244bf975b4298cad910aaa
master1234791 <80994374+master1234791@users.noreply.github.com>
2025-08-18 13:03:52 -0600

    Update connection.js

--- connection.js
+++ connection.js
@@ -7,19 +7,20 @@
  // Your web app's Firebase configuration
  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
  const firebaseConfig = {
-  apiKey: "AIzaSyBjh2WHyZZ1WaB2LVfMdq84BkC5mI00dEw",
-  authDomain: "activityloader.firebaseapp.com",
-  projectId: "activityloader",
-  storageBucket: "activityloader.appspot.com",
-  messagingSenderId: "604488222680",
-  appId: "1:604488222680:web:b04293ab53ef978162d4ad",
-  measurementId: "G-R6YNV1JSNY"
- };
+  apiKey: "AIzaSyCoqs-uC7DN5-lVLk79bbDvmM7AGAjYyT0",
+  authDomain: "teacheraid2-89ea5.firebaseapp.com",
+  projectId: "teacheraid2-89ea5",
+  storageBucket: "teacheraid2-89ea5.firebasestorage.app",
+  messagingSenderId: "421961067051",
+  appId: "1:421961067051:web:98abfd1993fefb1004186e"
+};
+
 
  // Initialize Firebase
  const app = initializeApp(firebaseConfig);
  const analytics = getAnalytics(app);
 
  export{app,analytics}
+
 
  console.log("Conexión a Firebase establecida correctamente.");
